06fd6f42533d8f42cb2eaaa7d94b6875371ae4ff
fangsida <fangsida@corp.netease.com>
2020-08-18 00:27:55 +0800

    208,95

---
+++ algorithm/src/main/java/com/netease/spring/demo/algorithm/Leetcode95.java
@@ -0,0 +1,14 @@
+package com.netease.spring.demo.algorithm;
+
+import java.util.List;
+
+/**
+ * @author fangsida
+ * @date 2020/8/18
+ */
+public class Leetcode95 {
+
+    public List<TreeNode> generateTrees(int n) {
+
+    }
+}
