5990f789d5edc7f11a6ad4d5bd60156e81e6d232
fangsida <fangsida@corp.netease.com>
2020-08-18 00:02:09 +0800

    今日3道题，173，437，951

---
+++ algorithm/src/main/java/com/netease/spring/demo/algorithm/Leetcode173.java
@@ -0,0 +1,47 @@
+package com.netease.spring.demo.algorithm;
+
+import java.util.ArrayDeque;
+
+/**
+ * @author fangsida
+ * @date 2020/8/16
+ */
+public class Leetcode173 {
+
+    public static class BSTIterator {
+
+        ArrayDeque<TreeNode> deque;
+
+        public BSTIterator(TreeNode root) {
+            deque = new ArrayDeque<>();
+
+            //二叉树的中序遍历
+            midVisit(root, deque);
+        }
+
+        private void midVisit(TreeNode root, ArrayDeque<TreeNode> deque) {
+            if (null == root) {
+                return;
+            }
+
+            midVisit(root.left, deque);
+            deque.offer(root);
+            midVisit(root.right, deque);
+
+        }
+
+        /**
+         * @return the next smallest number
+         */
+        public int next() {
+            return deque.poll().val;
+        }
+
+        /**
+         * @return whether we have a next smallest number
+         */
+        public boolean hasNext() {
+            return !deque.isEmpty();
+        }
+    }
+}
--- algorithm/src/main/java/com/netease/spring/demo/algorithm/TreeNode.java
+++ algorithm/src/main/java/com/netease/spring/demo/algorithm/TreeNode.java
@@ -1,5 +1,8 @@
 package com.netease.spring.demo.algorithm;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @author funstar
  * @date 2020/2/8
@@ -11,5 +14,31 @@ public class TreeNode {
 
     TreeNode(int x) {
         val = x;
+    }
+
+
+    public static TreeNode buildTree(Integer[] arr){
+        TreeNode root = new TreeNode(arr[0]);
+
+        Deque<TreeNode> deque  = new ArrayDeque<>();
+        deque.offer(root);
+        int i=0;
+        while (!deque.isEmpty()){
+            TreeNode node = deque.poll();
+            i++;
+            if (i<arr.length && null != arr[i]){
+                TreeNode left = new TreeNode(arr[i]);
+                node.left = left;
+                deque.offer(left);
+            }
+            i++;
+            if (i<arr.length && null != arr[i]){
+                TreeNode right = new TreeNode(arr[i]);
+                node.right = right;
+                deque.offer(right);
+            }
+        }
+
+        return root;
     }
 }
