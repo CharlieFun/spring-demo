af371de409b5bc06b261586e8cb4fa4afde0a140
fangsida <fangsida@corp.netease.com>
2020-08-21 00:46:37 +0800

    8.21 4道题

---
+++ algorithm/src/main/java/com/netease/spring/demo/algorithm/Leetcode145.java
@@ -0,0 +1,34 @@
+package com.netease.spring.demo.algorithm;
+
+import java.util.*;
+
+/**
+ * @author fangsida
+ * @date 2020/8/21
+ */
+public class Leetcode145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<>();
+        if (null == root) {
+            return res;
+        }
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.push(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.pop();
+            res.addFirst(node.val);
+            if (node.left != null) {
+                queue.push(node.left);
+            }
+            if (node.right != null) {
+                queue.push(node.right);
+            }
+
+        }
+
+        return res;
+    }
+
+}
---
+++ algorithm/src/main/java/com/netease/spring/demo/algorithm/Leetcode894.java
@@ -0,0 +1,39 @@
+package com.netease.spring.demo.algorithm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author fangsida
+ * @date 2020/8/20
+ */
+public class Leetcode894 {
+
+    Map<Integer,List<TreeNode>> cache = new HashMap<>();
+
+    public List<TreeNode> allPossibleFBT(int N) {
+        if (!cache.containsKey(N)){
+           List<TreeNode> ans = new ArrayList<>();
+           if (N==1){
+               ans.add(new TreeNode(0));
+           }else if (N %2 ==1){
+               for (int i=0;i<N;i++ ){
+                   for (TreeNode left:allPossibleFBT(i)){
+                       for (TreeNode right:allPossibleFBT(N-i-1)){
+                           TreeNode root = new TreeNode(0);
+                           root.left = left;
+                           root.right = right;
+                           ans.add(root);
+                       }
+                   }
+               }
+           }
+
+           cache.put(N,ans);
+        }
+
+        return cache.get(N);
+    }
+}
